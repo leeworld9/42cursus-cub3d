d0154bed49263da503ea5d162194cae8dddc83df
Doheon Lee <leeworld9@gmail.com>
2021-02-28 20:00:49 +0900

    dda

--- srcs/cub3d.h
+++ srcs/cub3d.h
@@ -6,14 +6,24 @@
 /*   By: dohelee <dohelee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/21 18:48:13 by dohelee           #+#    #+#             */
-/*   Updated: 2021/02/23 00:06:48 by dohelee          ###   ########.fr       */
+/*   Updated: 2021/02/25 18:10:31 by dohelee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
-#include "../minilibx-linux/mlx.h"
-#include "../libft/libft.h"
+# include "../minilibx-linux/mlx.h"
+# include "../libft/libft.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <math.h>
+
+# define KEY_ESC	65307
+# define KEY_W		119
+# define KEY_A		97
+# define KEY_S		115
+# define KEY_D		100
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -1,12 +1,241 @@
 #include "cub3d.h"
 
-int     main(void)
+#define  EPS            (1e-06)
+#define  is_zero(d)     (fabs(d) < EPS)
+#define  deg2rad(d)     ((d)*M_PI/180.0)    /* degree to radian */
+#define  rad2deg(d)     ((d)*180.0/M_PI)    /* radian to degree */
+#define  min(a,b)       ((a)<(b)? (a):(b))
+#define  max(a,b)       ((a)>(b)? (a):(b))
+
+#define  SX         800     /* screen width */
+#define  SY         600     /* screen height */
+#define  FOV        60      /* field of view (in degree) */
+#define  FOV_H      deg2rad(FOV)
+#define  FOV_V      (FOV_H*(double)SY/(double)SX)
+#define  WALL_H     1.0
+
+#define  MAPX   11
+#define  MAPY   15
+
+# define KEY_EVENT_PRESS    2
+
+static const double ANGLE_PER_PIXEL = FOV_H / (SX-1.);
+static const double FOVH_2 = FOV_H / 2.0;
+
+enum { VERT, HORIZ };
+
+typedef enum 
 {
-    void    *mlx;
-    void    *mlx_win;
+    DIR_N=0,
+    DIR_E,
+    DIR_W,
+    DIR_S
+}       dir_t;
 
-    mlx = mlx_init();
-    mlx_win = mlx_new_window(mlx, 800, 600, "Hello world!");
-    mlx_loop(mlx);
+static int map[MAPX][MAPY] = {
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1},
+    {1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1},
+    {1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+    };
 
-}    
+int map_get_cell( int x, int y )
+{
+    return (x >= 0 && x < MAPX && y >= 0 && y < MAPY) ? map[x][y] : -1;
+}
+
+int sgn( double d )
+{
+    return is_zero(d) ? 0 : ((d > 0) ? 1 : -1);
+}
+
+double l2dist( double x0, double y0, double x1, double y1 )
+{
+    double dx = x0 - x1;
+    double dy = y0 - y1;
+    return sqrt(dx*dx + dy*dy);
+}
+
+bool get_wall_intersection( double ray, double px, double py, dir_t* wdir, double* wx, double* wy )
+{
+    int xstep = sgn( cos(ray) );  /* +1 (right), 0 (no change), -1 (left) */
+    int ystep = sgn( sin(ray) );  /* +1 (up),    0 (no change), -1 (down) */
+
+    double xslope = (xstep == 0) ? INFINITY : tan(ray);
+    double yslope = (ystep == 0) ? INFINITY : 1./tan(ray);
+
+    double nx = (xstep > 0) ? floor(px)+1 : ((xstep < 0) ? ceil(px)-1 : px);
+    double ny = (ystep > 0) ? floor(py)+1 : ((ystep < 0) ? ceil(py)-1 : py);
+
+    printf("\nray=%.2f deg, xstep=%d, ystep=%d, xslope=%.2f, yslope=%.2f, nx=%.2f, ny=%.2f\n",
+        rad2deg(ray), xstep, ystep, xslope, yslope, nx, ny);
+
+    double f=INFINITY, g=INFINITY;
+    bool hit = false;
+    int hit_side; /* either VERT or HORIZ */
+
+    while( !hit )
+    {
+        int mapx, mapy;
+
+        if( xstep != 0 ) f = xslope * (nx-px) + py;
+        if( ystep != 0 ) g = yslope * (ny-py) + px;
+
+        /* which is nearer to me - VERT(nx,f) or HORIZ(g,ny)? */
+        double dist_v = l2dist(px, py, nx, f);
+        double dist_h = l2dist(px, py, g, ny);
+
+        if( dist_v < dist_h ) { /* VERT is nearer; go along x-axis */
+            mapx = (xstep == 1) ? (int)(nx) : (int)(nx)-1 ;
+            mapy = (int) f;
+            hit_side = VERT;
+            printf(" V(%d, %.2f) ->", mapx, f);
+        }
+        else {  /* HORIZ is nearer; go along y-axis */
+            mapx = (int) g;
+            mapy = (ystep == 1) ? (int)(ny) : (int)(ny)-1 ;
+            hit_side = HORIZ;
+            printf(" H(%.2f, %d) ->", g, mapy);
+        }
+        int cell = map_get_cell(mapx, mapy);
+        if( cell < 0 ) break;   /* out of map */
+
+        if( cell == 1 ) {   /* hit wall? */
+            if( hit_side == VERT ) {
+                *wdir = (xstep > 0) ? DIR_W : DIR_E;
+                *wx = nx;
+                *wy = f;
+            }
+            else { /* HORIZ */
+                *wdir = (ystep > 0) ? DIR_S : DIR_N;
+                *wx = g;
+                *wy = ny;
+            }
+            hit = true;
+            printf(" hit wall!\n");
+            break;
+        }
+
+        if( hit_side == VERT ) nx += xstep;
+        else ny += ystep;
+    }
+    /* end of while(!hit) */
+
+    return hit;
+}
+
+double cast_single_ray( int x, double px, double py, double th, dir_t* pdir)
+{
+    double ray = (th + FOVH_2) - (x * ANGLE_PER_PIXEL);
+
+    dir_t wdir;     /* direction of wall */
+    double wx, wy;  /* coord. of wall intersection point */
+
+    if( get_wall_intersection(ray, px, py, &wdir, &wx, &wy) == false )
+        return INFINITY; /* no intersection - maybe bad map? */
+
+    double wdist = l2dist(px, py, wx, wy);
+    wdist *= cos(th - ray);  /* 보정 */
+
+    return wdist;
+}
+
+///////////////////////////////
+
+typedef struct    s_img
+{
+    void        *img;
+    int            *data;
+    int            bpp;
+    int            line_size;
+    int            endian;
+}                t_img;
+ 
+typedef struct    s_game
+{
+    void        *mlx;
+    void        *win;
+    int            map[MAPX][MAPY];
+    t_img        img;
+	double px;
+	double py;
+	double th;
+}               t_game;
+
+
+int get_wall_height( double dist )
+{
+    double fov_h = 2.0 * dist * tan(FOV_V/2.0);
+    return (int)(SY * (WALL_H / fov_h)); /* in pixels */
+}
+
+void draw_wall( t_game *game, double wdist, int x, int color )
+{
+    int wh = get_wall_height(wdist);    /* wall height, in pixels */
+
+    /* starting/ending y pos of the wall slice */
+    int y0 = (int)((SY - wh)/2.0);
+    int y1 = y0 + wh - 1;
+
+    /* needs clipping */
+    int ystart = max(0, y0);
+    int yend = min(SY-1, y1);
+
+	//바닥
+	for( int y=yend; y<SY; y++ )
+	{
+		game->img.data[SX * y + x] = 0xC0C0C0;
+	}
+
+	//벽
+	for( int y=ystart; y<yend; y++ )
+	{
+		game->img.data[SX * y + x] = 0x000000+x;
+	}
+
+	//printf("\nwh: %d, y0 : %d, y1 : %d, ystart : %d , yend : %d\n", wh, y0, y1, ystart, yend);
+}
+
+int        draw_loop(t_game *game)
+{
+
+    mlx_put_image_to_window(game->mlx, game->win, game->img.img, 0, 0);
+    return (0);
+}
+
+int main( int ac, char** av )
+{
+    if( ac != 4 ) {
+        fprintf(stderr,"usage: %s x y th(deg)\n", av[0]);
+        exit(1);
+    }
+    t_game	game;
+	game.mlx = mlx_init();
+    game.win = mlx_new_window(game.mlx, SX, SY, "cub3D");
+
+    double px, py, th;
+    game.px = atof(av[1]);
+    game.py = atof(av[2]);
+    game.th = deg2rad(atof(av[3]));
+
+    game.img.img = mlx_new_image(game.mlx, SX, SY);
+    game.img.data = (int *)mlx_get_data_addr(game.img.img, &game.img.bpp, &game.img.line_size, &game.img.endian);
+
+	for( int x=0; x<SX; x++ ) {
+        dir_t wdir;
+        double wdist = cast_single_ray(x, game.px, game.py, game.th, &wdir);
+		draw_wall(&game, wdist, x, 0xFF0000);  
+    }
+
+    mlx_loop_hook(game.mlx, draw_loop, &game);
+ 
+    mlx_loop(game.mlx);
+    return (0);
+}
